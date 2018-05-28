2cbc7108116e160f2a1ef40fead59d9f45bee6e2
Monika <apple@MacBook-Air-apple-2.local>
2018-05-28 23:53:31 +0200

    added monaco code editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NgxEditorModel } from 'ngx-monaco-editor';
 
 @Component({
   selector: 'sl-root',
-  templateUrl: './app.component.html',
+  templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'sl';
+  editorOptions = {theme: 'vs-dark', language: 'javascript'};
+  code = 'function x() {\n\tconsole.log("Hello world!");\n}';
+  jsonCode = [
+    '{',
+    '    "p1": "v3",',
+    '    "p2": false',
+    '}'
+  ].join('\n');
+
+  model: NgxEditorModel = {
+    value: this.jsonCode,
+    language: 'json',
+    uri: 'foo.json'
+  };
+  public onInit(editor) {
+      const line = editor.getPosition();
+      console.log(line);
+    }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  onMonacoLoad: () => {
+    const id = 'foo.json';
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [{
+        uri: 'http://myserver/foo-schema.json',
+        fileMatch: [id],
+        schema: {
+          type: 'object',
+          properties: {
+            p1: {
+              enum: [ 'v1', 'v2']
+            },
+            p2: {
+              $ref: 'http://myserver/bar-schema.json'
+            }
+          }
+        }
+      }, {
+        uri: 'http://myserver/bar-schema.json',
+        fileMatch: [id],
+        schema: {
+          type: 'object',
+          properties: {
+            q1: {
+              enum: [ 'x1', 'x2']
+            }
+          }
+        }
+      }]
+    });
+  }
+};
 
 
 @NgModule({
@@ -12,7 +49,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    MonacoEditorModule.forRoot(monacoConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
