153d2b3341ba6db991c9feaaad7280045ea47bfc
tomasz-stelagowski <tomasz.stelagowski@gmail.com>
2018-05-29 03:20:08 +0200

    added transision slides

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { Slide1Component } from './slides/slide-1/slide-1.component';
+import { Slide2Component } from './slides/slide-2/slide-2.component';
+
+const routes: Routes = [
+  { path: 'slide1', component: Slide1Component, data: { state: '1'} },
+  { path: 'slide2', component: Slide2Component, data: { state: '2' } },
+  { path: '', redirectTo: '/slide1', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SlideComponent } from './components/slide/slide.component';
+import { Slide1Component } from './slides/slide-1/slide-1.component';
+import { Slide2Component } from './slides/slide-2/slide-2.component';
+import { EventsService } from './services/events.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideComponent,
+    Slide1Component,
+    Slide2Component
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [EventsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
